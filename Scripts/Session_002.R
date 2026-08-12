2f680378f631dcc7fa99ef6f4e0e693ab60e3224
Tahminaazam <tahminaazam588@gmail.com>
2026-08-13 02:05:08 +0600

    added new scripts and files

---
+++ Scripts/Session_002.R
@@ -0,0 +1,64 @@
+2+2
+6/3
+5-3
+# Variables
+# Create a variable using left_ward operator (<-)
+# ver_name <- value
+a <- 10
+a+2
+# Variable naming convention
+Age <- 10
+myage <- 27
+my age <- 27
+my_age <- 27
+# Data types
+# Numbers
+
+myage <- 27
+class(myage)
+height <- 5.3
+class(height)
+
+# Text/Characters
+
+Name <- "Tahmina"
+class(Name)
+
+# Logical (True/False)
+is_weekday <- FALSE
+class(is_weekday)
+
+# Arithmetic operators
+num1 <- 10
+num2 <- 2
+
+num1+num2
+num1-num2
+num1*num2
+num1/num2
+# If remainder == 0: even, if not: odd
+10 %% 2
+11%%2
+6%%3
+7%%5
+
+# Comparative operators (True/False)
+2==2
+2<3
+2>3
+2<=3
+2>=3
+2>=2
+2<=3
+# Multiple condirions
+# And
+2 == 2 && 2 < 4
+2 == 2 && 2 > 4
+# OR
+2 == 2 | 2 < 4
+2 == 2 | 2 > 4
+3==4 | 5<2
+# Not
+2==!2
+3==!4
+!2==!2
